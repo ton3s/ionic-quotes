523f5546b19013a7cca306fa7b0c5c071320f809
Antonio Cunanan <antonio.cunanan@gmail.com>
2018-07-21 17:41:42 -0500

    feat: Add about page.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FavoritesPage} from '../favorites/favorites';
 import {SearchPage} from '../search/search';
 import {HomePage} from '../home/home';
+import {AboutPage} from "../about/about";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,6 +12,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = FavoritesPage;
   tab3Root = SearchPage;
+  tab4Root = AboutPage;
 
   constructor() {
   }
