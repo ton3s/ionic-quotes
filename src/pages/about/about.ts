4e6a997e108d8cf4dbb6db79c55e7d44aaeb392d
Antonio Cunanan <antonio.cunanan@gmail.com>
2018-07-22 11:30:05 -0500

    feat: Updated background colors.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {UtilityProvider} from "../../providers/utility/utility.provider";
 
-/**
- * Generated class for the AboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-about',
